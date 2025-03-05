c330b998e29176a389b264ad837defda0a4f7ef3
felipemellopadula <fpadula1985@outlook.com>
2025-03-04 21:19:00 -0300

    mudanças iguais m5

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,25 @@
-import startServer from './server';
+import express from 'express';
+import "reflect-metadata";
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes';
+import errorHandler from './middlewares/errorHandler';
+import cors from "cors";
+
+
+dotenv.config();
 
 // Inicia o servidor
-startServer();
+
+export const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Rotas
+app.use('/api/auth', authRoutes);
+
+// Middleware de tratamento de erros
+app.use(errorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,8 @@
-import express from 'express';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes';
-import errorHandler from './middlewares/errorHandler';
-import cors from "cors";
+import { app } from "./app";
 
-
-dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares globais
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
 
-// Rotas
-app.use('/api/auth', authRoutes);
-
-// Middleware de tratamento de erros
-app.use(errorHandler);
-
-// Inicialização do servidor
-const startServer = () => {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
-};
-
-export default startServer;
+app.listen(PORT, () => {
+  console.log(`API sucessfully started at port ${PORT}`);
+});
